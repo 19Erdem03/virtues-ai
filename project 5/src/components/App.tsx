37d85ccf58fb944731eb7cbddff86daca225ca09
19Erdem03 <erdemerolsuer@gmail.com>
2025-10-11 14:09:00 +0300

    Add files via upload

---
+++ project 5/src/components/App.tsx
@@ -0,0 +1,6 @@
+@@ .. @@
+      <Hero onBookCall={handleBookCall} />
+      <Features />
+      <Services onBookCall={handleBookCall} />
+-      <Footer onCookiePolicy={handleCookiePolicy} onPrivacyPolicy={handlePrivacyPolicy} onTermsOfService={handleTermsOfService} />
++      <Footer onCookiePolicy={handleCookiePolicy} onPrivacyPolicy={handlePrivacyPolicy} onTermsOfService={handleTermsOfService} onBookCall={handleBookCall} />
